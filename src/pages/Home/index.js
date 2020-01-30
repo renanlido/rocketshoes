af3b298ec438f94badbdd2705cf5ce4fb3dace89
Renan Oliveira <renanlido@yahoo.com.br>
2020-01-30 14:38:27 -0300

    Update - Refatoring actions for redux saga

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -38,10 +38,10 @@ class Home extends Component {
     this.setState({ products: newProduct });
   }
 
-  handleAddProduct = product => {
-    const { addToCart } = this.props;
+  handleAddProduct = id => {
+    const { addToCartRequest } = this.props;
 
-    addToCart(product);
+    addToCartRequest(id);
   };
 
   render() {
@@ -58,7 +58,7 @@ class Home extends Component {
 
             <button
               type="button"
-              onClick={() => this.handleAddProduct(product)}
+              onClick={() => this.handleAddProduct(product.id)}
             >
               <div>
                 <MdAddShoppingCart size={16} color="#fff" />
